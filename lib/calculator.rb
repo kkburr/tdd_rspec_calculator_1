aadc96b7f178051b4ee1ac6b1e80822c5ebfcf99
Kaitlin Barrer <kaitlin.barrer@gmail.com>
2014-02-22 11:01:46 -0700

    Adding all changes in tdd_rspec_calculator file

---
+++ lib/calculator.rb
@@ -0,0 +1,17 @@
+class Calculator
+  def initialize
+    @total = 0
+  end
+
+  def total
+    @total
+  end
+  def add(number)
+    @total += number
+    self
+  end
+  def subtract(number)
+    @total -= number
+    self
+  end
+end
--- spec/lib/calculator_spec.rb
+++ spec/lib/calculator_spec.rb
@@ -1,4 +1,48 @@
 require './lib/calculator'
 
 describe Calculator do
+
+  describe ".total" do
+    it "should be 0 on a new Calculator" do
+      expect(subject.total).to eq(0)
+    end
+  end
+
+  describe ".add" do
+    it "should add a number to the total" do
+      subject.add(5)
+      expect(subject.total).to eq(5)
+    end
+    it "should handle multiple calls to add" do
+      subject.add(5).add(2)
+      expect(subject.total).to eq(7)
+    end
+    it "should handle negative numbers" do
+      subject.add(-2)
+      expect(subject.total).to be(-2)
+    end
+    it "should handle floats" do
+      subject.add(2.5)
+      expect(subject.total).to eq(2.5)
+    end
+  end
+
+  describe ".subtract" do
+    it "should subtract a number from the total" do
+      subject.subtract(5)
+      expect(subject.total).to eq(-5)
+    end
+    it "should handle multiple calls to subtract" do
+      subject.subtract(2).subtract(5)
+      expect(subject.total).to eq(-7)
+    end
+    it "should handle negative numbers" do
+      subject.subtract(-5)
+      expect(subject.total).to eq(5)
+    end
+    it "should handle floats" do
+      subject.subtract(2.5)
+      expect(subject.total).to eq(-2.5)
+    end
+  end
 end
